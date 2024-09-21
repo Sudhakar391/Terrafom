a73649d4fba704d17253b2a132506f20e8f9b53b
sudhakar391 <gtrs391@gmail.com>
2024-09-21 22:14:22 +0530

    tt

--- EKS/variables.tf
+++ EKS/variables.tf
@@ -2,7 +2,7 @@
 variable "cluster_name" {
   description = "The name of the EKS cluster"
   type        = string
-  default     = "my-eks-cluster"
+  default     = "my-eks-cluster1"
 }
 
 variable "node_instance_type" {
