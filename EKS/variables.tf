6f6c6024a453c7bb67daec503be348e2bd1db0f0
sudhakar391 <gtrs391@gmail.com>
2024-09-21 21:36:41 +0530

    tt

--- EKS/variables.tf
+++ EKS/variables.tf
@@ -28,6 +28,59 @@ variable "min_size" {
   type        = number
   default     = 2
 }
+
+variable "vpc_name" {
+  description = "The name of the VPC."
+  type        = string
+  default     = "main-vpc"
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC."
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "private_subnets" {
+  description = "List of CIDR blocks for private subnets."
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "public_subnets" {
+  description = "List of CIDR blocks for public subnets."
+  type        = list(string)
+  default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
+
+variable "cluster_name" {
+  description = "The name of the EKS cluster."
+  type        = string
+  default     = "my-eks-cluster"
+}
+
+variable "k8s_version" {
+  description = "The Kubernetes version for the EKS cluster."
+  type        = string
+  default     = "1.21"
+}
+
+variable "instance_type" {
+  description = "The EC2 instance type for worker nodes."
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "key_name" {
+  description = "The name of the EC2 key pair."
+  type        = string
+}
+
+variable "bastion_security_group_id" {
+  description = "Security group ID for the Bastion host."
+  type        = string
+}
+
 variable "access_key" {
   description = "AWS access key"
 }
