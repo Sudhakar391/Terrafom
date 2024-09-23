1ba9c52cccec28bbebbea23a42b68a9c04fdfe22
sudhakar391 <taraka.personal@gmail.com>
2024-09-23 17:47:50 +0530

    Updated code

--- EKS/outputs.tf
+++ EKS/outputs.tf
@@ -1,18 +1,18 @@
 
-output "eks_endpoint" {
-  value = module.eks.cluster_endpoint
-}
+# output "eks_endpoint" {
+#   value = module.eks.cluster_endpoint
+# }
 
-output "eks_cluster_name" {
-  value = module.eks.cluster_id
-}
+# output "eks_cluster_name" {
+#   value = module.eks.cluster_id
+# }
 
-output "eks_cluster_security_group_id" {
-  value = module.eks.cluster_security_group_id
-}
+# output "eks_cluster_security_group_id" {
+#   value = module.eks.cluster_security_group_id
+# }
 
-output "kubeconfig" {
-  value     = module.eks.kubeconfig
-  sensitive = true
-}
+# output "kubeconfig" {
+#   value     = module.eks.kubeconfig
+#   sensitive = true
+# }
 
