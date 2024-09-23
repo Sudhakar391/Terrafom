0fe3e6de365ade0947b826cbc44c887bbf57b5a9
Sudhakar69 <76212832+Sudhakar69@users.noreply.github.com>
2024-09-23 14:05:50 +0530

    Update variables.tf

--- EKS/variables.tf
+++ EKS/variables.tf
@@ -2,7 +2,7 @@
 variable "cluster_name" {
   description = "The name of the EKS cluster"
   type        = string
-  default     = "my-eks-cluster1"
+  default     = "my-eks-cluster123"
 }
 
 variable "node_instance_type" {
